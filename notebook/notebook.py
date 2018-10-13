2540972088695e3392aa9a24f0141daffe0a630c
bennieregenold <bennieregenold@gmail.com>
2018-10-13 09:54:30 -0500

    Revert "bug fix"
    This reverts commit 3caf9ba7469c563b10e3e300155bccb0c0ef0d54.

--- notebook/notebook.py
+++ notebook/notebook.py
@@ -37,17 +37,13 @@ class Notebook:
     def _find_note(self, note_id):
         '''Locate the note with a give id'''
         for note in self.notes:
-            if str(note.id) == str(note_id):
+            if note.id == note_id:
                 return note
         return None
 
     def modify_memo(self, note_id, memo):
         '''Find the note with an id and change its memo.'''
-        note = self._find_note(note_id)
-        if note:
-            note.memo = memo
-            return True
-        return False
+        self._find_note(note_id).memo = memo
 
     def modify_tags(self, note_id, tags):
         '''Find the note with given id and change its tags.'''
